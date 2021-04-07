de82469b93d8411daf1084251ccec4baf460ad03
Matheus-souza-araujo <matheus1mrs@gmail.com>
2021-04-06 22:56:50 -0300

    reestruturaçao das pastas

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from "express"; // import em vez de require
 
 import { categoriesRoutes } from "./routes/categories.routes"; // importando nossa rota para o server
+import { specificationsRoutes } from "./routes/specification.routes";
 
 const app = express();
 
@@ -8,7 +9,7 @@ app.use(express.json());
 
 app.use("/categories", categoriesRoutes); // colocando nossa rota em uso no servidor
 // passando o /categories não será mais necessário passar o path nos routes
-
+app.use("/specifications", specificationsRoutes);
 app.listen(3333, () => {
   console.log("Server is Running!");
 });
