356b41fc8d3f4aba3a2bd13667883f58348631e8
Matheus-souza-araujo <matheus1mrs@gmail.com>
2021-12-06 20:54:27 -0300

    criptografando senha dos usuarios

--- src/modules/accounts/useCases/createUser/CreateUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUseCase.ts
@@ -1,3 +1,4 @@
+import { hash } from "bcrypt";
 import { inject, injectable } from "tsyringe";
 
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
@@ -16,10 +17,18 @@ class CreateUserUseCase {
     password,
     name,
   }: ICreateUserDTO): Promise<void> {
+    const userAlreadyExists = await this.usersRepository.findByEmail(email);
+
+    if (userAlreadyExists) {
+      throw new Error("User already exists");
+    }
+
+    // criptografando a senha com bcrypt
+    const passwordHash = await hash(password, 8);
     await this.usersRepository.create({
       driver_license,
       email,
-      password,
+      password: passwordHash,
       name,
     });
   }
